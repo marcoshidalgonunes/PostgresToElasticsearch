d2b52f508eabfb5ade2a546cf44a957a2dfdcd82
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2024-12-27 16:18:31 -0300

    Initial commit

---
+++ agregator/src/test/java/com/postgrestoelasticsearch/agregator/agregator/AgregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgrestoelasticsearch.agregator.agregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

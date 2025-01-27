40fe6f71ebdbb837fd03f5d171f8280615eefa68
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2025-01-27 15:22:00 -0300

    Using Http verbs corresponding to the operations

--- agregator/src/main/java/com/postgrestoelasticsearch/agregator/controllers/AgregatorController.java
+++ agregator/src/main/java/com/postgrestoelasticsearch/agregator/controllers/AgregatorController.java
@@ -8,23 +8,24 @@ import com.postgrestoelasticsearch.agregator.services.AgregatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/agregator")
 public class AgregatorController {
 
     @Autowired
     private AgregatorService service;
     
-    @GetMapping("/create")
+    @PostMapping
     public Summary create() {
         return service.create();
     }
 
-    @GetMapping("/delete")
+    @DeleteMapping()
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> delete() {
         service.delete();
